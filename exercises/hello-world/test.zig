919c3ed876222bc081ae10998b62dad464f04fa4
Peter Tseng <pht24@cornell.edu>
2020-11-23 01:44:15 +0000

    hello-world: use expectEqualStrings (in zig 0.7.0)

--- exercises/hello-world/test.zig
+++ exercises/hello-world/test.zig
@@ -2,8 +2,5 @@ const testing = @import("std").testing;
 const hello_world = @import("./hello_world.zig");
 
 test "greet" {
-    // TODO: can use std.testing.expectEqualStrings
-    // Added on April 30 in https://github.com/ziglang/zig/pull/4683
-    // probably next release of Zig.
-    testing.expectEqualSlices(u8, "Hello, world!", hello_world.say());
+    testing.expectEqualStrings("Hello, world!", hello_world.say());
 }
